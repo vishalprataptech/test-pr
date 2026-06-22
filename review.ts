f656993e5dda01e4bf7bf90cbcd7758db08aee79
Vishal Pratap Singh <vpsingh3445@gmail.com>
2026-06-22 13:46:55 +0530

    master ji

--- review.ts
+++ review.ts
@@ -43,3 +43,27 @@ if (!repoSyncRecord) {
         }
 
         await step.run("save-vectors-to-pinecone", async () => {
+
+            import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/src/index"; // your drizzle instance
+import { nextCookies } from "better-auth/next-js";
+import * as schema from "@/src/db/schema";
+export const auth = betterAuth({
+   database: drizzleAdapter(db, {
+        provider: "pg", // or "mysql", "sqlite"
+        schema,
+    }),
+     socialProviders: { 
+    github: { 
+      clientId: process.env.GITHUB_CLIENT_ID as string, 
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+
+       mapProfileToUser:async(profile)=>({
+        email:profile.email ?? `${profile.id}@users.noreply.github.com`,
+        name:profile.name ?? profile.login,
+      })
+    }, 
+  }, 
+   plugins: [nextCookies()] // make sure this is the last plugin in the array 
+});
